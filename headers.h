585a6a244efae683f8acc6b507bc6ede7a667548
Jared Moulton <jaredmoulton3@gmail.com>
2020-07-29 23:26:21 -0600

    Add full maze-solving capability

--- headers.h
+++ headers.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <csignal>
 #include <iostream>
 #include <optional>
 #include <vector>
--- main.cpp
+++ main.cpp
@@ -13,7 +13,22 @@ class SimplePoint {
         y = ay;
         val = aval;
     }
+    bool in(std::vector<SimplePoint> array) {
+        for (const SimplePoint &point : array) {
+            if (point.x == x && point.y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
 };
+bool operator==(const SimplePoint &lhs, const SimplePoint &rhs) {
+    return (lhs.x == rhs.x && lhs.y == rhs.y);
+}
+std::ostream &operator<<(std::ostream &os, const SimplePoint &m) {
+    return os << "(" << m.x << "," << m.y << ")"
+              << " " << m.val << endl;
+}
 
 class Point : public SimplePoint {
    public:
@@ -36,7 +51,8 @@ class Point : public SimplePoint {
         val = temp.val;
         simple = SimplePoint(temp.x, temp.y, temp.val);
     }
-    vector<SimplePoint> getNeighbors(vector<vector<int>> *maze) {
+
+    vector<SimplePoint> getNeighbors(const vector<vector<int>> *maze) {
         vector<SimplePoint> temp;
         if (x != 0) {
             temp.push_back(SimplePoint(x - 1, y, (*maze)[y][x - 1]));
@@ -54,16 +70,19 @@ class Point : public SimplePoint {
     }
 };
 
+std::ostream &operator<<(std::ostream &os, Point &m) {
+    return os << "(" << m.simple.x << "," << m.simple.y << ")"
+              << " " << m.val << endl
+              << m.neighbors.size();
+}
+
 template <typename T>
 class Node {
    public:
     Node *pPrev, *pNext;
     T value;
 
-    Node(T aValue) : value(aValue) {
-        pNext = this;
-        // value = aValue;
-    }
+    Node(T aValue) : value(aValue) {}
 };
 
 template <typename T>
@@ -84,21 +103,21 @@ class Queue {
         count = 1;
     }
 
-    T *pop() {
+    std::optional<T> pop() {
         if (count == 0) {
-            return nullptr;
+            return nullopt;
         }
         count -= 1;
         Node<T> *temp = pHead;
         pHead = temp->pNext;
-        T *data = &temp->value;
+        T data = temp->value;
         delete temp;
         return data;
     }
 
     void append(T aTail) {
         Node<T> *temp = new Node<T>(aTail);
-        if (pTail == nullptr) {
+        if (count == 0) {
             temp->pPrev = temp;
             temp->pNext = temp;
             pHead = temp;
@@ -124,11 +143,44 @@ const vector<vector<int>> maze1 = {
     {0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0},
 };
 
+const vector<vector<int>> maze6 = {
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};  // Answer 39
+
+class Timer {
+   public:
+    std::__1::chrono::steady_clock::time_point m_StartTime;
+    Timer() { auto m_StartTime = std::chrono::high_resolution_clock::now(); }
+    ~Timer() { stop(); }
+    void stop() {
+        std::cout << std::chrono::high_resolution_clock::now() - m_StartTime
+                  << endl;
+    }
+}
+
 int answer(vector<vector<int>> maze) {
     Point startPoint = Point(0, 0, 0);
     startPoint.neighbors = startPoint.getNeighbors(&maze);
     Queue<Point> queue(startPoint);
-
     while (!queue.isEmpty()) {
         Point activePoint = *queue.pop();
         activePoint.history.push_back(activePoint.simple);
@@ -136,15 +188,19 @@ int answer(vector<vector<int>> maze) {
             activePoint.y == maze.size() - 1) {
             return activePoint.history.size();
         }
-        for (const SimplePoint &neighbor : activePoint.neighbors) {
-            Point temp = Point(neighbor);
-            temp.history = activePoint.history;
-            temp.neighbors = temp.getNeighbors(&maze);
-            if (neighbor.val == 0) {
-                temp.saldo = activePoint.saldo;
-            } else {
-                if (activePoint.saldo >= 1) {
-                    temp.saldo += 1;
+        for (SimplePoint &neighbor : activePoint.neighbors) {
+            if (!neighbor.in(activePoint.history)) {
+                Point temp = Point(neighbor);
+                temp.history = activePoint.history;
+                temp.neighbors = temp.getNeighbors(&maze);
+                if (neighbor.val == 0) {
+                    temp.saldo = activePoint.saldo;
+                    queue.append(temp);
+                } else {
+                    if (activePoint.saldo < 1) {
+                        temp.saldo += 1;
+                        queue.append(temp);
+                    }
                 }
             }
         }
@@ -152,4 +208,4 @@ int answer(vector<vector<int>> maze) {
     return 100000;
 }
 
-int main() { std::cout << answer(maze1) << endl; }
+int main() { std::cout << answer(maze6) << endl; }
