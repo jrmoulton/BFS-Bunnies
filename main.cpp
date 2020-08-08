c4d0dc5d5b106eeb667444767bfe108a3f0373e9
Jared Moulton <jaredmoulton3@gmail.com>
2020-08-08 15:43:52 -0600

    Separate the test cases

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 #include "headers.h"
-using namespace std;
+#include "testCases.h"
 
 class SimplePoint {
    public:
@@ -27,17 +27,14 @@ bool operator==(const SimplePoint &lhs, const SimplePoint &rhs) {
 }
 std::ostream &operator<<(std::ostream &os, const SimplePoint &m) {
     return os << "(" << m.x << "," << m.y << ")"
-              << " " << m.val << endl;
+              << " " << m.val << std::endl;
 }
 
 class Point : public SimplePoint {
    public:
     int saldo = 0;
     SimplePoint simple;
-    vector<SimplePoint> neighbors;
-    vector<SimplePoint> history;
-
-    // Point() { return; }
+    std::vector<SimplePoint> neighbors, history;
 
     Point(int ax, int ay, int aval) {
         x = ax;
@@ -52,8 +49,9 @@ class Point : public SimplePoint {
         simple = SimplePoint(temp.x, temp.y, temp.val);
     }
 
-    vector<SimplePoint> getNeighbors(const vector<vector<int>> *maze) {
-        vector<SimplePoint> temp;
+    std::vector<SimplePoint> getNeighbors(
+        const std::vector<std::vector<int>> *maze) {
+        std::vector<SimplePoint> temp;
         if (x != 0) {
             temp.push_back(SimplePoint(x - 1, y, (*maze)[y][x - 1]));
         }
@@ -72,37 +70,9 @@ class Point : public SimplePoint {
 
 std::ostream &operator<<(std::ostream &os, Point &m) {
     return os << "(" << m.simple.x << "," << m.simple.y << ")"
-              << " " << m.val << endl
+              << " " << m.val << std::endl
               << m.neighbors.size();
 }
-
-const vector<vector<int>> maze1 = {
-    {0, 0, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 0}, {1, 1, 1, 1, 1, 1},
-    {0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0},
-};
-
-const vector<vector<int>> maze6 = {
-    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-};  // Answer 39
 
 class Timer {
    public:
@@ -123,14 +93,14 @@ class Timer {
         auto us = end - start;
         double ms = us * 0.001;
         double s = ms * 0.0001;
-        std::cout << "ms: " << ms << endl;
+        std::cout << "ms: " << ms << std::endl;
     }
 
    private:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTime;
 };
 
-int answer(vector<vector<int>> maze) {
+int answer(std::vector<std::vector<int>> maze) {
     Timer timer;
     Point startPoint = Point(0, 0, 0);
     startPoint.neighbors = startPoint.getNeighbors(&maze);
@@ -163,4 +133,4 @@ int answer(vector<vector<int>> maze) {
     return 100000;
 }
 
-int main() { answer(maze6); }
+int main() { std::cout << answer(maze6) << std::endl; }
